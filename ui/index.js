9b7830c573fffcb65681ab08a5dcfde4455f04bc
Jed Fox <git@twopointzero.us>
2017-02-02 17:21:21 -0500

    Use SE Chat events; add flag links to load older messages

--- ui/index.js
+++ ui/index.js
@@ -1,8 +1,6 @@
 const handle = require('./messages')
 
-let roomId
 setTimeout(() => {
-  roomId = +window.location.href.match(/\/rooms\/(\d+)\//)[1]
   const { shell } = require('electron')
   window.addEventListener('load', () => {
     const $ = window.jQuery
@@ -42,25 +40,7 @@ setTimeout(() => {
       }
     }
     handle.addButtons()
+    $('#getmore, #getmore-mine').click(handle.addButtons)
+    window.CHAT.addEventHandlerHook(handle)
   })
 })
-
-const init = (ws) => {
-  ws.addEventListener('message', ({ data }) => {
-    data = JSON.parse(data)
-    const room = data['r' + roomId]
-    if (room.e) {
-      for (const event of room.e) {
-        handle(event)
-      }
-    }
-  })
-}
-
-const _ws = window.WebSocket
-window.WebSocket = function WebSocket (...args) {
-  const ws = new _ws(...args)
-  init(ws)
-  window.WebSocket = _ws
-  return ws
-}
