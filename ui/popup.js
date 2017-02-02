073b31e999463e4307d4c0ae31af083bdccf23a3
Jed Fox <git@twopointzero.us>
2017-02-02 16:45:05 -0500

    Handle deleted questions properly.

--- ui/popup.js
+++ ui/popup.js
@@ -1,5 +1,8 @@
 const clickFlagLink = $ => {
   // run in the context of the opened window.
+  if (window.location.href === 'data:text/html,chromewebdata') {
+    window.close()
+  }
   if (!window.location.hash.length) {
     // Q
     $('#question .flag-post-link').click()
@@ -23,8 +26,16 @@ const onLoad = (f) => (() => {
 }).toString().replace('$$f', String(f))
 
 const addUserScripts = require('./user-scripts')
-module.exports = url => {
+module.exports = (url, onDeleted = x => x) => {
   const _w = window.open(url)
+  _w.blur()
   _w.eval(`(${onLoad(addUserScripts)})()`)
   _w.eval(`(${onLoad(clickFlagLink)})()`)
+  setTimeout(() => {
+    if (_w.closed) {
+      onDeleted()
+    } else {
+      _w.focus()
+    }
+  }, 500)
 }
